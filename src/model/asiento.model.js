29a5a888ab0738391fbd491b8ab6a63a72feee90
Esteban <luigirp64@gmail.com>
2021-10-27 20:29:18 -0600

    arreglo de conflicto con ObjectId

--- src/model/asiento.model.js
+++ src/model/asiento.model.js
@@ -1,22 +1,17 @@
 module.exports = (mongoose) => {
-    var asientoSchema = mongoose.Schema({
-      ID_Fila: {
-        require: true,
-        type: String,
-      },
-      ID_Asiento: {
-        type: String,
-        required: true,
-      },
-      ID_Vuelo: {
-        require: true,
-        type: String,
-      },
-      ID_Tiquete: {
-        require: true,
-        type: [{ type: mongoose.Schema.Types.ObjectId, 
-          ref: "tiquete" }],
-      },
-    });
-    return mongoose.model("asiento", asientoSchema);
-  };
+  var asientoSchema = mongoose.Schema({
+    Fila: {
+      require: true,
+      type: String,
+    },
+    ID_Vuelo: {
+      require: false,
+      type: [{ type: String, ref: "vuelo" }],
+    },
+    ID_Tiquete: {
+      require: false,
+      type: [{ type: String, ref: "tiquete" }],
+    },
+  });
+  return mongoose.model("asiento", asientoSchema);
+};
